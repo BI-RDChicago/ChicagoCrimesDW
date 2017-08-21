a22bc949a222a73c7c49c1c941e2c4a62b014b51
Guilherme <guigaa@gmail.com>
2017-08-21 01:33:50 -0300

    Readequação dos metodos filter_apply para model Crimes e outras melhorias de leitura

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -16,94 +16,45 @@ class CrimesController < ApplicationController
 		@comm_id = comm_id
 		@groupby = groupby
 		community_area_description(@comm_id)
-
 	end
 
-	def community_area_description(comm_id)
-		
-		if @comm_id != "0"
-			@community_area_desc = CommunityArea.find(comm_id).description
-		else
+	def community_area_description(comm_id)		
+		if @comm_id == "0"
 			@community_area_desc = "All"
+		else
+			@community_area_desc = CommunityArea.find(comm_id).description
 		end
 	end
 
 	def community_filter
-
 		@community_areas = CommunityArea.all.order("description")
-
 	end
 
 	def time_filter
-
 		@community_areas = CommunityArea.all.order("description")
-
 	end
 
 	def location_filter
-
 		@community_areas = CommunityArea.all.order("description")
-
 	end
 
 	def community_filter_apply
-
 		filters(params[:start], params[:finish], params[:arrest], params[:comm_id], params[:groupby])		
 
-		@community_grouped = Crime.community_filter_scope(@start, @finish, @arrest, @comm_id, @groupby)
+		@community_filtered = Crime.community_filter_scope(@start, @finish, @arrest, @comm_id, @groupby)
 
 	end
 
 	def time_filter_apply
-
 		filters(params[:start], params[:finish], params[:arrest], params[:comm_id], params[:groupby])		
-
-		if @groupby == "yyyy"
-			if @arrest == "all"
-				scope = Crime.select("to_char(date, 'yyyy') as start" , "sum(quantity) as qnt").group("to_char(date, 'yyyy'),to_char(date, 'yyyy')").order("sum(quantity) desc")
-			else
-				scope = Crime.select("to_char(date, 'yyyy') as start" , "sum(arrest_qnt) as qnt").group("to_char(date, 'yyyy'),to_char(date, 'yyyy')").order("sum(arrest_qnt) desc")
-			end
-		else
-			if @arrest == "all"
-				scope = Crime.select("to_char(date, 'MM/yyyy') as start" , "sum(quantity) as qnt").group("to_char(date, 'MM/yyyy'),to_char(date, 'yyyy/MM')").order("sum(quantity) desc")
-			else
-				scope = Crime.select("to_char(date, 'MM/yyyy') as start" , "sum(arrest_qnt) as qnt").group("to_char(date, 'MM/yyyy'),to_char(date, 'yyyy/MM')").order("sum(arrest_qnt) desc")
-			end
-		end
-
-		scope = scope.joins(:CommunityArea)		
-
-		if @comm_id != "0"
-			scope = scope.where(CommunityArea: @comm_id)
-		end
-
-		scope = scope.joins(:DimTime)
-		scope = scope.where("dim_times.date" => @start..@finish)
-
-		@time_filtered = scope
+		
+		@time_filtered = Crime.time_filter_scope(@start, @finish, @arrest, @comm_id, @groupby)
 	end
-
 
 	def location_filter_apply
 
 		filters(params[:start], params[:finish], params[:arrest], params[:comm_id], params[:groupby])		
 
-		if @arrest == "all"
-			scope = Crime.select("dim_locations.name", "sum(quantity) as qnt").order("sum(quantity) desc")
-		else
-			scope = Crime.select("dim_locations.name", "sum(arrest_qnt) as qnt").order("sum(arrest_qnt) desc")
-		end
-
-		scope = scope.joins(:location).group("dim_locations.name") 
-
-		if @comm_id != "0"
-			scope = scope.where(CommunityArea: @comm_id)
-		end
-
-		scope = scope.joins(:DimTime)
-		scope = scope.where("dim_times.date" => @start..@finish)
-
-		@location_filtered = scope
+		@location_filtered = Crime.location_filter_scope(@start, @finish, @arrest, @comm_id, @groupby)
 	end
 end
--- app/models/crime.rb
+++ app/models/crime.rb
@@ -22,4 +22,52 @@ class Crime < ApplicationRecord
 
 		@community_grouped = scope
   end
+
+  def self.time_filter_scope(start, finish, arrest, comm_id, groupby)
+  	if groupby == "yyyy"
+			if arrest == "all"
+				scope = Crime.select("to_char(date, 'yyyy') as start" , "sum(quantity) as qnt").group("to_char(date, 'yyyy'),to_char(date, 'yyyy')").order("sum(quantity) desc")
+			else
+				scope = Crime.select("to_char(date, 'yyyy') as start" , "sum(arrest_qnt) as qnt").group("to_char(date, 'yyyy'),to_char(date, 'yyyy')").order("sum(arrest_qnt) desc")
+			end
+		else
+			if arrest == "all"
+				scope = Crime.select("to_char(date, 'MM/yyyy') as start" , "sum(quantity) as qnt").group("to_char(date, 'MM/yyyy'),to_char(date, 'yyyy/MM')").order("sum(quantity) desc")
+			else
+				scope = Crime.select("to_char(date, 'MM/yyyy') as start" , "sum(arrest_qnt) as qnt").group("to_char(date, 'MM/yyyy'),to_char(date, 'yyyy/MM')").order("sum(arrest_qnt) desc")
+			end
+		end
+
+		scope = scope.joins(:CommunityArea)
+		scope = scope.joins(:DimTime)		
+
+		unless comm_id == "0"
+			scope = scope.where(CommunityArea: comm_id)
+		end
+		scope = scope.where("dim_times.date" => start..finish)
+
+		@time_filtered = scope
+  end
+
+  def self.location_filter_scope(start, finish, arrest, comm_id, groupby)
+  	if arrest == "all"
+			scope = Crime.select("dim_locations.name", "sum(quantity) as qnt").order("sum(quantity) desc")
+		else
+			scope = Crime.select("dim_locations.name", "sum(arrest_qnt) as qnt").order("sum(arrest_qnt) desc")
+		end
+
+		scope = scope.joins(:location).group("dim_locations.name") 
+		scope = scope.joins(:DimTime)
+
+		if comm_id != "0"
+			scope = scope.where(CommunityArea: comm_id)
+		end
+		scope = scope.where("dim_times.date" => start..finish)
+
+		@location_filtered = scope
+  end
+
+
+
+
 end
